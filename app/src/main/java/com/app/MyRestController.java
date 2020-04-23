1b9bc45ad567ac9b095961ea7420f9c4058bd3dc
Tomeshwar Shiwane <Tomeshwar Shiwane@IB-Pune-LAP-052>
2020-04-24 04:48:56 +0530

    added rest controller.

---
+++ app/src/main/java/com/app/MyRestController.java
@@ -0,0 +1,16 @@
+package com.app;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+	
+	public MyRestController() {
+		  System.out.println("MyRestController");
+	}
+	
+	
+	
+	
+
+}
