30ec5bddf3063aa8b0d718849f28578373de0eeb
Tomeshwar Shiwane <Tomeshwar Shiwane@IB-Pune-LAP-052>
2020-04-24 04:32:11 +0530

    Added the log 4j object

--- app/src/main/java/com/app/AppApplication.java
+++ app/src/main/java/com/app/AppApplication.java
@@ -2,12 +2,17 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @SpringBootApplication
 public class AppApplication {
+	static Logger logger = LoggerFactory.getLogger(AppApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+	
+		logger.info("Application Stared Success.... ");
+	
 	}
 
 }
